edca0616588808f65111a04556384f3aaed61fb0
Kirill Novak <hermes.coder@gmail.com>
2016-01-27 19:55:49 +0300

    set index page for /

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 // MongoDB
-mongoose.connect('mongodb://localhost/rest_test');
+mongoose.connect('mongodb://localhost/donetoday');
 
 // Express
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.set('view engine', 'jade');
 
 // Routes
 app.use('/api', require('./routes/api'));
+app.use('/', function(req, res){
+	res.render('index', { title: 'DoneToday', message: 'Welcome to your personal life changer.'});
+});
 
 // Start server
-app.listen(3000);
-console.log('API is running on port 3000');
+app.listen(4001);
+console.log('API is running on port 4001');
