cf7c95a08bf4131adf4e203a788453f868e7719e
Kirill Novak <hermes.coder@gmail.com>
2016-01-31 20:40:35 +0300

    post request for records

--- models/record.js
+++ models/record.js
@@ -1,11 +1,17 @@
-var restful = require('node-restful');
-var mongoose = restful.mongoose;
+// var restful = require('node-restful');
+// var mongoose = restful.mongoose;
+
+var mongoose = require('mongoose');
+
 
 // Schema
 var recordSchema = new mongoose.Schema({
-	name: String,
-	sku: String,
-	price: Number
+	message: String,
+	creationDate: {
+		type: Date,
+		default: Date.now
+	}
 });
 
-module.exports = restful.model('Records', recordSchema);
+// module.exports = restful.model('Records', recordSchema);
+module.exports = mongoose.model('Records', recordSchema);
--- server.js
+++ server.js
@@ -2,23 +2,48 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
+var router = express.Router();
+
+// Models
+var Record = require('./models/record.js');
+
 // MongoDB
 mongoose.connect('mongodb://127.0.0.1/donetoday');
 
 // Express
 var app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ 
+	extended: true 
+}));
 app.use(bodyParser.json());
 app.set('view engine', 'jade');
 
 // Routes
-app.use('/api', require('./routes/api'));
+// app.use('/api', require('./routes/api'));
 app.use('/', function(req, res){
 	res.render('index', { title: 'DoneToday', message: 'Welcome to your personal life changer.'});
 });
+var recordsRoute = router.route('/records');
+
+// Create endpoint /api/beers for POSTS
+recordsRoute.post(function(req, res) {
+  // Create a new instance of the Beer model
+  var record = new Record();
+
+  record.message = req.body.message;
+  record.creationDate = req.body.creationDate;
+
+  record.save(function(err) {
+    if (err) {
+      res.send(err);
+		}
+
+    res.json({ message: 'Record added', data: record });
+  });
+});
 
 // Tasks
-app.use('/tasks', require('./routes/tasks'));
+// app.use('/tasks', require('./routes/tasks'));
 
 // Clients
 // app.use('/clients', require('./routes/clients')());
